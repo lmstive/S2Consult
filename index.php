2eebccd3639ce4fd29ddf7c20979044f72f7d34a
Luis Miguel <lmstive@gmail.com>
2025-03-21 14:24:59 -0300

    Arruando o index

--- index.php
+++ index.php
@@ -5,15 +5,15 @@
  */
 
 // Incluir a página inicial
-include_once 'pages/index.php';
+//include_once 'pages/index.php';
 
 /**
  * Alternativa: Redirecionamento
  * Caso prefira manter as páginas em uma estrutura separada e redirecionar,
  * descomente o código abaixo e comente a linha acima.
  */
-/*
+
 header("Location: pages/index.php");
 exit;
-*/
+
 ?>
